83160b60a8b50295534f419b143ba4e20ab6fd42
dave-cybercat <paskalleo@yandex.ru>
2021-06-19 18:58:26 +0300

    try to parse json

--- DefenitionGetters/WikiDefinitionGetter.ts
+++ DefenitionGetters/WikiDefinitionGetter.ts
@@ -1,11 +1,18 @@
 import { IDefinitionGetter, Definition } from './../types/DefinitionGetter';
+const axios = require('axios').default //or i can replace by "import"?
+
 
 export class WikiDefinitionGetter implements IDefinitionGetter{
     
-    public sourceName : string = "Wikipedia";
+    public sourceName : string = "Wikipedia"; //for what?
 
     public getDefinition(word : string) : Definition{
-        
+        axios.get(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${word}&format=json`)
+		.then(function(response: string) {
+			//response = JSON.parse(response)
+			//console.log(response["data"]["batchcomplete"])
+			console.log(response)
+		})
         // Алгоритм по получению информации из вики
         return {
             content : "Default definition :)", 
